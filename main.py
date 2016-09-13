63fd51f28a839b5a997131c93f5be872d1241a4b
Tommy Li <dfplayer95@yahoo.com>
2016-09-13 15:44:45 -0700

    feat: Added data.py for querying data

--- main.py
+++ main.py
@@ -5,45 +5,25 @@ from sklearn.tree import DecisionTreeRegressor
 from IPython.display import display # Allows the use of display() for DataFrames
 import matplotlib.pyplot as plt
 import matplotlib
+import data as stock
+from parse import *
 
-# Show matplotlib plots inline (nicely formatted in the notebook)
 
 def run():
-	matplotlib.style.use('ggplot')
-	# Load the wholesale customers dataset
-	try:
-	    data = pd.read_csv("customers.csv")
-	    data.drop(['Region', 'Channel'], axis = 1, inplace = True)
-	    print "Wholesale customers dataset has {} samples with {} features each.".format(*data.shape)
-	except:
-	    print "Dataset could not be loaded. Is the dataset missing?"
-
-	# TODO: Select three indices of your choice you wish to sample from the dataset
-	indices = [1,2,6]
-
-	# Create a DataFrame of the chosen samples
-	samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
-	print "Chosen samples of wholesale customers dataset:"
-	display(samples)
-	display(data.describe())
-
-	# TODO: Make a copy of the DataFrame, using the 'drop' function to drop the given feature
-	new_data = data.drop(['Detergents_Paper'], 1)
-
-	# TODO: Split the data into training and testing sets using the given feature as the target
-	X_all = new_data
-	y_all = data[['Detergents_Paper']]
-	X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.25, random_state=0)
-
-	# TODO: Create a decision tree regressor and fit it to the training set
-	regressor = DecisionTreeRegressor(random_state=0)
-	regressor.fit(X_train,y_train)
-
-	# TODO: Report the score of the prediction using the testing set
-	score = regressor.score(X_test,y_test)
-	print "Regressor R2 score: {:.4f}.".format(score)
-	pd.scatter_matrix(data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
-	plt.show()
+	file = open("input.txt", "r")
+	inputs = file.read()
+	ticker = search('Ticker symbols:{}\n',inputs)
+	start_date = search('Start_date:{}\n',inputs)
+	end_date = search('End_date:{}\n',inputs)
+	tickers = ticker[0].split(",")
+	print tickers
+	for tick in tickers:
+		tick_ = tick.replace(' ','')
+		print "This is the data for " + tick_
+		data = stock.getData(tick_,start_date[0],end_date[0],"monthly","default")
+		print data.head()
+	file.close()
+	
 
 if __name__ == '__main__':
     run()
